7268819581aa29636cd7f61cb0562e40d9f74f9a
rahulrajeev312 <rahulrajeev312@gmail.com>
2021-05-16 10:20:16 -0700

    changes done

--- src/pages/report.js
+++ src/pages/report.js
@@ -15,9 +15,9 @@ function Report(){
     return(
         <div className= "all">
         <div className= "buttonbkg">
-        <button2 className = "report-button" onClick = {onclick2}>report1</button2>
+        <button2 className = {visible === 1? 'report-button active-btn' : 'report-button'} onClick = {onclick2}>report1</button2>
         
-        <button2 className = "report-button" onClick = {onclick3}>report2</button2>
+        <button2 className =  {visible === 2? 'report-button active-btn' : 'report-button'} onClick = {onclick3}>report2</button2>
         
         </div>
         { visible === 1 && <iframe width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMTc0NDEwNzctOTI1OS00ZmZjLTg4ZTktM2YxYTBjNDhiYjBkIiwidCI6ImYxYWExNTQ4LWFjYTgtNDFjYS04MDlmLThlMDcwODM0YmViZCJ9&pageName=ReportSection2a208ffdd513709c5237" frameborder="0" allowFullScreen="true"></iframe>}
