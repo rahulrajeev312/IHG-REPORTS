1641e57d5b77ac2c0738c69375361e99ed04e4f4
rahulrajeev312 <rahulrajeev312@gmail.com>
2021-05-18 08:16:40 -0700

    all changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/login";
 import report from "./pages/report";
+const TITLE = 'My Page Title'
 
 function App(){
+
+  
   return(
     <Router>
       <Route component={Login} exact path="/"/>
